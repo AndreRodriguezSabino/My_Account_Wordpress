5126e74b3f49928dcc7c7e4b442513bd40eca275
André Rodríguez Sabino <andresabinocalvo@gmail.com>
2021-03-03 13:51:31 +0000

    Create my_account_wordpress.php

---
+++ my_account_wordpress.php
@@ -0,0 +1,14 @@
+<?php
+// My Account - User Name
+function my_user_name() {
+  $current_user = wp_get_current_user();
+  return trim( 'Welcome ' . $current_user->user_firstname . ' ' . $current_user->user_lastname );
+}
+add_shortcode( 'my-user-name', 'my_user_name' );
+
+// URL Logout
+function my_url_logout() {
+  return wp_logout_url();
+}
+add_shortcode( 'my-url-logout', 'my_url_logout' );
+?>
